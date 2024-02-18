6978ef54489aa110f297f4e7601110f7aaf35d2f
aayman01 <ayman.alfi098@gmail.com>
2024-02-18 19:48:31 +0600

    doing js work

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -2,20 +2,32 @@ function scrollToSection() {
   document.getElementById('ticket-section').scrollIntoView({ behavior: 'smooth' });
 }
 
-const allBtn = document.getElementsById('add-btn');
+const allBtn = document.querySelectorAll('.btn')
 let count = 1;
+let select = 0;
 let clickCount = 0;
 for(const btn of allBtn){
 // console.log(btn)
-    btn.addEventListener('click',function(){
-        
+    btn.addEventListener('click',function(e){
         if(clickCount < 4){
+            console.log(e.target.innerText);
             const allSeat = document.getElementById('seat-count').innerText;
             let allSeatText = parseInt(allSeat);
             allSeatText -=count
             setInnerText('seat-count', allSeatText);
             clickCount++;
-            setBackgroundColorById('add-btn')
+            select = select + 1;
+            setInnerText('seat-select',select);
+            const seatName = e.target.innerText;
+            const selectedContainer = document.getElementById('slected-ticket-container')
+            const tr = document.createElement('tr');
+            const td = document.createElement('td').innerText=seatName;
+            const td2 = document.createElement('td').innerText = 'Economoy';
+            const td3 = document.createElement('td').innerText = '500';
+            tr.appendChild(td);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            selectedContainer.appendChild(tr)
         }
 
     })
@@ -30,5 +42,5 @@ function setInnerText (id,value){
 function setBackgroundColorById (elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-[#1DD100]');
-    element.classList.add('text-white')
+    element.classList.add('text-white');
 }
