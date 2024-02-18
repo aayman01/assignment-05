811c97aa17130b03610c4bf5666575d401e782e6
aayman01 <ayman.alfi098@gmail.com>
2024-02-18 20:50:55 +0600

    working on js

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -6,6 +6,7 @@ const allBtn = document.querySelectorAll('.btn')
 let count = 1;
 let select = 0;
 let clickCount = 0;
+let ticketPrice = 550;
 for(const btn of allBtn){
 // console.log(btn)
     btn.addEventListener('click',function(e){
@@ -18,16 +19,33 @@ for(const btn of allBtn){
             clickCount++;
             select = select + 1;
             setInnerText('seat-select',select);
+
+            // show ticket list
             const seatName = e.target.innerText;
-            const selectedContainer = document.getElementById('slected-ticket-container')
+
+            const selectedContainer = document.querySelector('.slected-ticket-container')
             const tr = document.createElement('tr');
-            const td = document.createElement('td').innerText=seatName;
-            const td2 = document.createElement('td').innerText = 'Economoy';
-            const td3 = document.createElement('td').innerText = '500';
+            const td = document.createElement('td');
+            td.innerText=seatName;
+            const td2 = document.createElement('td');
+            td2.innerText = 'Economoy'
+            const td3 = document.createElement('td');
+            td3.innerText = '500';
             tr.appendChild(td);
             tr.appendChild(td2);
             tr.appendChild(td3);
-            selectedContainer.appendChild(tr)
+            selectedContainer.appendChild(tr);
+
+            // total-cost
+
+            const totalCost = document.getElementById('total-cost').innerText;
+            const totalCostConvert = parseInt(totalCost);
+            const totalPrice = document.getElementById('total-cost').innerText = totalCostConvert + ticketPrice;
+
+            // grand cost
+
+            
+            
         }
 
     })
